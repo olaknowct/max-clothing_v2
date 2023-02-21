3e480fd8fc26465cb7f957d449cc017e4c3f1108
Christopher Olano <olanochristopher@gmail.com>
2023-02-21 21:09:22 +0800

    react route outlet use case

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './routes/home/home.component';
 
+const Shop = () => {
+  return <h1>i am the shope page</h1>;
+};
+
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={<Home />} />
+      <Route path='/home' element={<Home />}>
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
