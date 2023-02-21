d4d65f8667864b06cd0b4ec2a81b66c7f6d06b11
Christopher Olano <olanochristopher@gmail.com>
2023-02-21 21:32:18 +0800

    out and index react router dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Outlet } from 'react-router-dom';
 import Home from './routes/home/home.component';
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
 
 const Shop = () => {
   return <h1>i am the shope page</h1>;
@@ -8,7 +19,8 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={<Home />}>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
       </Route>
     </Routes>
