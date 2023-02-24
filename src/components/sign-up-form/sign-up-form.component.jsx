dac01e9d128203828048969452d15101baecd8ad
Christopher Olano <olanochristopher@gmail.com>
2023-02-24 16:43:21 +0800

    Bring up button component and styles for sign up

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import { createAuthUserWithEmailAndPassowrd, createUserDocumentFromAuth } from '../../utils/firebase.utils';
+import Button from '../button/button.component';
 import FormInput from '../form-input/form-input.component';
+
+import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
   displayName: '',
@@ -44,8 +47,9 @@ const SignUpForm = () => {
   };
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className='sign-up-container'>
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
       <form action='' onSubmit={handleSubmit}>
         <FormInput required label='Display Name' type='text' onChange={handleChange} name='displayName' value={displayName} />
 
@@ -54,7 +58,7 @@ const SignUpForm = () => {
         <FormInput required label='Password' type='password' onChange={handleChange} name='password' value={password} />
 
         <FormInput required label='Confirm Password' type='password' onChange={handleChange} name='confirmPassword' value={confirmPassword} />
-        <button type='submit'>Sign up</button>
+        <Button type='submit'>Sign up</Button>
       </form>
     </div>
   );
