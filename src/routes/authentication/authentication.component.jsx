02b2718fabe7801f8cd79cc87562e2ef3ed074e0
Christopher Olano <olanochristopher@gmail.com>
2023-02-25 11:19:47 +0800

    use authentication for signup and signin feature

---
+++ src/routes/authentication/authentication.component.jsx
@@ -0,0 +1,15 @@
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase.utils';
+
+const Authentication = () => {
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      <SignInForm />
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default Authentication;
