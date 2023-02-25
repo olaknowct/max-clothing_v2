5e7305907cf2a804c9955b6f7d6d08102ab2f1b8
Christopher Olano <olanochristopher@gmail.com>
2023-02-25 11:20:31 +0800

    create signin component

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import { signInWithGooglePopup, createUserDocumentFromAuth, signAuthUserWithEmailAndPassword } from '../../utils/firebase.utils';
+import Button from '../button/button.component';
+import FormInput from '../form-input/form-input.component';
+
+import './sign-in-form.styles.scss';
+
+const defaultFormFields = {
+  email: '',
+  password: '',
+};
+
+const SignInForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const signInWithGoogle = async () => {
+    const { user } = await signInWithGooglePopup();
+    await createUserDocumentFromAuth(user);
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await signAuthUserWithEmailAndPassword(email, password);
+      resetFormFields();
+    } catch (error) {}
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div className='sign-up-container'>
+      <h2>Already have an account?</h2>
+      <span>Sign in with your email and password</span>
+      <form action='' onSubmit={handleSubmit}>
+        <FormInput required label='Email' type='email' onChange={handleChange} name='email' value={email} />
+        <FormInput required label='Password' type='password' onChange={handleChange} name='password' value={password} />
+        <div className='buttons-container'>
+          <Button type='submit'>Sign In</Button>
+          <Button buttonType='google' onClick={signInWithGoogle}>
+            Google Sign In
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignInForm;
