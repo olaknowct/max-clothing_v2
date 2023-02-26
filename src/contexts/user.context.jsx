a0d6dadf051cddd04ba23304b28959d6fe954ad3
Christopher Olano <olanochristopher@gmail.com>
2023-02-26 09:31:06 +0800

    create observer on auth state change

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+import { onAuthStateChangedListener, signOutUser } from '../utils/firebase.utils';
 
 // as the actual value you want to access
 export const UserContext = createContext({
@@ -10,5 +11,18 @@ export const UserContext = createContext({
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const value = { currentUser, setCurrentUser };
+
+  signOutUser();
+
+  // unsubcribe once run
+  useEffect(() => {
+    // call back will run every time there is changes from our auth
+    // checks if user are authenticate
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      console.log(user);
+    });
+
+    return unsubscribe;
+  }, []);
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
