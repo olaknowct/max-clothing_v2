4652e29817299784da039c6c21adc78decc774d0
Christopher Olano <olanochristopher@gmail.com>
2023-02-27 20:53:02 +0800

    fix indentation nav component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -9,6 +9,7 @@ import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase.utils';
 
 import './navigation.styles.scss';
+
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
   const { isCartOpen } = useContext(CartContext);
