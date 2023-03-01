3bf304116606b0b48f1990149271e57bd316cd82
Christopher Olano <olanochristopher@gmail.com>
2023-03-01 19:30:47 +0800

    add category  component and route

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import Category from '../../components/category/category.component';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import './shop.styles.scss';
 
@@ -6,6 +7,7 @@ const Shop = () => {
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
     </Routes>
   );
 };
