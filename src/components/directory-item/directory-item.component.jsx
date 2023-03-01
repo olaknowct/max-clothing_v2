933171f2f14fac16960c27984ccc8d2d3e843fff
Christopher Olano <olanochristopher@gmail.com>
2023-03-01 21:35:35 +0800

    use CSS modules in directory item component

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,11 +1,15 @@
-import './directory-item.styles.scss';
+import styles from './directory-item.module.scss';
+// import './directory-item.styles.scss';
 
 const DirectoryItem = ({ category }) => {
   const { imageUrl, title } = category;
   return (
-    <div className='directory-item-container'>
-      <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }}></div>
-      <div className='body'>
+    <div className={styles['directory-item-container']}>
+      <div
+        className={styles['background-image']}
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
+      <div className={styles['body']}>
         <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
