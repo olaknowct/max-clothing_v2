b7826556643d0d21c2dcd6c108053d36026004ce
Christopher Olano <olanochristopher@gmail.com>
2023-03-02 00:12:53 +0800

    use navigate to navigate through each categories

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,11 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import { BackgroundImage, Body, DirectoryItemContainer } from './directory-item.styles';
 // import styles from './directory-item.module.scss';
 // import './directory-item.styles.scss';
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
+
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl}></BackgroundImage>
       <Body>
         <h2>{title}</h2>
