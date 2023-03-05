b85bf107da5a898844cd85b7a18e9134a01e1913
Christopher Olano <olanochristopher@gmail.com>
2023-03-05 21:53:59 +0800

    wrapping shop by categories context

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,14 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import Category from '../../components/category/category.component';
+import { CategoriesProvider } from '../../contexts/categories.context';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import './shop.styles.scss';
 
 const Shop = () => {
   return (
-    <Routes>
-      <Route index element={<CategoriesPreview />} />
-      <Route path=':category' element={<Category />} />
-    </Routes>
+    <CategoriesProvider>
+      <Routes>
+        <Route index element={<CategoriesPreview />} />
+        <Route path=':category' element={<Category />} />
+      </Routes>
+    </CategoriesProvider>
   );
 };
 
