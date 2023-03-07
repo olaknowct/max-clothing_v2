98431cfac6bc4c7d71285be1fe771e68f39522d2
Christopher Olano <olanochristopher@gmail.com>
2023-03-07 12:35:58 +0800

    use redux to store categoriesmap use useeffect to dispatch one time the action

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,10 +1,25 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import Category from '../../components/category/category.component';
 import { CategoriesProvider } from '../../contexts/categories.context';
+import { setCategoriesMap } from '../../store/categories/category.action';
+import { getCategoriesAndDocuments } from '../../utils/firebase.utils';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import './shop.styles.scss';
 
 const Shop = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      dispatch(setCategoriesMap(categoryMap));
+    };
+
+    getCategoriesMap();
+  }, [dispatch]);
+
   return (
     <CategoriesProvider>
       <Routes>
