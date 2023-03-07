19cfbcd326f35e5ef5f5a715f5cc9844a6d6c8f2
Christopher Olano <olanochristopher@gmail.com>
2023-03-07 23:42:42 +0800

    creating categories memoize using reselector libraries

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,9 +1,20 @@
-export const selectCategoriesMap = (state) => {
-  console.log('Selector fired');
-  const categoriesMap = state.categories.categories.reduce((acc, category) => {
+import { createSelector } from 'reselect';
+
+const selectCategoryReducer = (state) => {
+  return state.categories;
+};
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => {
+    return categoriesSlice.categories;
+  } // the only this function will run is the only when the first argument is now different
+);
+
+export const selectCategoriesMap = createSelector([selectCategories], (categories) => {
+  return categories.reduce((acc, category) => {
     const { title, items } = category;
     acc[title.toLowerCase()] = items;
     return acc;
   }, {});
-  return categoriesMap;
-};
+});
