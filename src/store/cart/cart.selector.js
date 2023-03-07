9f15eccaa03b8d49cfb9e28828e66151110e0392
Christopher Olano <olanochristopher@gmail.com>
2023-03-08 00:46:28 +0800

    migrating cart context to redux PT2 Memoize cart selector remove cart context

--- src/store/cart/cart.selector.js
+++ src/store/cart/cart.selector.js
@@ -1,8 +1,21 @@
 import { createSelector } from 'reselect';
 
-const newCartTotal = newCartItems.reduce(
-  (total, cartItem) => total + cartItem.quantity * cartItem.price,
-  0
-);
+const selectCartReducer = (state) => {
+  return state.cart;
+};
 
-const newCartCount = newCartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
+export const selectCartItems = createSelector([selectCartReducer], (cart) => {
+  return cart.cartItems;
+});
+
+export const selectIsCartOpen = createSelector([selectCartReducer], (cart) => {
+  return cart.isCartOpen;
+});
+
+export const selectCartCount = createSelector([selectCartItems], (cartItems) => {
+  return cartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0);
+});
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) => {
+  return cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
+});
