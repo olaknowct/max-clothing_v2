e6ea310d70be735d481721ab1a83d3960fa58ac3
Christopher Olano <olanochristopher@gmail.com>
2023-03-08 16:56:56 +0800

    use redux-thunk to handle async function : fetch category

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,8 +3,7 @@ import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import Category from '../../components/category/category.component';
 import { CategoriesProvider } from '../../contexts/categories.context';
-import { setCategories } from '../../store/categories/category.action';
-import { getCategoriesAndDocuments } from '../../utils/firebase.utils';
+import { fetchCategoriesAsync } from '../../store/categories/category.action';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import './shop.styles.scss';
 
@@ -12,13 +11,7 @@ const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments();
-
-      dispatch(setCategories(categoriesArray));
-    };
-
-    getCategoriesMap();
+    dispatch(fetchCategoriesAsync());
   }, [dispatch]);
 
   return (
