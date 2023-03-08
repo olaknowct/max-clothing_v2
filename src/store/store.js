5080397c135f5402d9900e960c05eb9a434df691
Christopher Olano <olanochristopher@gmail.com>
2023-03-08 12:52:28 +0800

    optimizing logger middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,22 +1,9 @@
 import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
+// import { loggerMiddleware } from './middleware/logger';
 import { rootReducer } from './root-reducer';
-
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log('Action type: ', action.type);
-  console.log('payload: ', action.payload);
-  console.log('currentState: ', store.getState());
-
-  next(action);
-
-  console.log('next state: ', store.getState());
-};
 
 const persistConfig = {
   key: 'root',
@@ -26,7 +13,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares = [loggerMiddleware];
+const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean); //tecnique : see redux-devtools section how
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 // Root-reducer
