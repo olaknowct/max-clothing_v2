0944518214fc2ce7245adb69d5d04920e3cbd1b0
Christopher Olano <olanochristopher@gmail.com>
2023-03-08 16:30:07 +0800

    setting up redux thunk middleware in store

--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,7 @@ import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 // import { loggerMiddleware } from './middleware/logger';
 import { rootReducer } from './root-reducer';
+import thunk from 'redux-thunk';
 
 const persistConfig = {
   key: 'root',
@@ -13,7 +14,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean); //tecnique : see redux-devtools section how
+const middlewares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean); //tecnique : see redux-devtools section how
 
 const composeEnhancers =
   (process.env.NODE_ENV !== 'production' &&
