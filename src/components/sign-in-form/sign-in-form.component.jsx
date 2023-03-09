2376a1b8d06acce79d887b8f7910ad6e791bea7d
Christopher Olano <olanochristopher@gmail.com>
2023-03-10 01:07:28 +0800

    redux-saga: email and google sign in in sagas

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { emailSignInStart, googleSignInStart } from '../../store/user/user.actions';
 
-import {
-  signInWithGooglePopup,
-  signAuthUserWithEmailAndPassword,
-} from '../../utils/firebase.utils';
+import { signAuthUserWithEmailAndPassword } from '../../utils/firebase.utils';
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 import FormInput from '../form-input/form-input.component';
 
@@ -15,6 +14,7 @@ const defaultFormFields = {
 };
 
 const SignInForm = () => {
+  const dispatch = useDispatch();
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { email, password } = formFields;
 
@@ -23,13 +23,14 @@ const SignInForm = () => {
   };
 
   const signInWithGoogle = async () => {
-    await signInWithGooglePopup();
+    dispatch(googleSignInStart());
   };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      await signAuthUserWithEmailAndPassword(email, password);
+      dispatch(emailSignInStart(email, password));
       resetFormFields();
     } catch (error) {
       switch (error.code) {
--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -1,5 +1,10 @@
 import { takeLatest, all, call, put } from 'redux-saga/effects';
-import { createUserDocumentFromAuth, getCurrentUser } from '../../utils/firebase.utils';
+import {
+  createUserDocumentFromAuth,
+  getCurrentUser,
+  signAuthUserWithEmailAndPassword,
+  signInWithGooglePopup,
+} from '../../utils/firebase.utils';
 import { signInFailed, signInSuccess } from './user.actions';
 import { USER_ACTION_TYPES } from './user.types';
 
@@ -7,6 +12,23 @@ export function* getSnapshotFromUserAuth(userAuth, additionalDetail) {
   try {
     const userSnapshot = yield call(createUserDocumentFromAuth, userAuth, additionalDetail); // same as createUserDocumentFromAuth(auth, details)
     yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+  } catch (error) {
+    yield put(signInFailed(error));
+  }
+}
+
+export function* signInWithEmail({ payload: { email, password } }) {
+  try {
+    const { user } = yield call(signAuthUserWithEmailAndPassword, email, password);
+    yield call(getSnapshotFromUserAuth, user);
+  } catch (error) {
+    yield put(signInFailed(error));
+  }
+}
+export function* signInWithGoogle() {
+  try {
+    const { user } = yield call(signInWithGooglePopup);
+    yield call(getSnapshotFromUserAuth, user);
   } catch (error) {
     yield put(signInFailed(error));
   }
@@ -22,10 +44,18 @@ export function* isUserAuthenticated() {
   }
 }
 
+export function* onGoogleSignInStart() {
+  yield takeLatest(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START, signInWithGoogle);
+}
+
 export function* onCheckUserSession() {
   yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated);
 }
 
+export function* onEmailSignInStart() {
+  yield takeLatest(USER_ACTION_TYPES.EMAIL_SIGN_IN_START, signInWithEmail);
+}
+
 export function* userSagas() {
-  yield all([call(onCheckUserSession)]);
+  yield all([call(onCheckUserSession), call(onGoogleSignInStart), call(onEmailSignInStart)]);
 }
