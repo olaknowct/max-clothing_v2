2a6db03e59d2c7e0cdfc141b63782d7751f8c520
Christopher Olano <olanochristopher@gmail.com>
2023-03-10 18:49:59 +0800

    getting started with REDUX TOOLKIT : base setup

--- src/store/store.js
+++ src/store/store.js
@@ -1,29 +1,33 @@
-import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { configureStore } from '@reduxjs/toolkit';
+// import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux';
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
-// import { loggerMiddleware } from './middleware/logger';
+// // import { loggerMiddleware } from './middleware/logger';
 import { rootReducer } from './root-reducer';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  blacklist: ['user'], // array of values you dont want to persist
-};
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+//   blacklist: ['user'], // array of values you dont want to persist
+// };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean); //tecnique : see redux-devtools section how
 
-const composeEnhancers =
-  (process.env.NODE_ENV !== 'production' &&
-    window &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
+// const composeEnhancers =
+//   (process.env.NODE_ENV !== 'production' &&
+//     window &&
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+//   compose;
 
-const composedEnhancers = composeEnhancers(applyMiddleware(...middlewares));
+// const composedEnhancers = composeEnhancers(applyMiddleware(...middlewares));
 
-// Root-reducer
-export const store = createStore(persistedReducer, undefined, composedEnhancers);
+// // Root-reducer
+export const store = configureStore({
+  reducer: rootReducer, // points to reducer
+  middleware: middlewares, // has default middleware but if declared then default are overlapped
+});
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
