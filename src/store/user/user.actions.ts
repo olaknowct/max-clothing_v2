4fce874faa7f793c34dfd6a1278de83834c90ad9
Christopher Olano <olanochristopher@gmail.com>
2023-03-21 23:29:28 +0800

    typing user actions convert ts create with matcher on action creator

---
+++ src/store/user/user.actions.ts
@@ -0,0 +1,92 @@
+import {
+  createAction,
+  withMatcher,
+  ActionWithPayload,
+  Action,
+} from '../../utils/reducer/reducer.utils';
+import { USER_ACTION_TYPES } from './user.types';
+import { UserData, AdditionalInformation } from '../../utils/firebase.utils';
+
+export type CheckUserSession = Action<USER_ACTION_TYPES.CHECK_USER_SESSION>;
+
+export type SetCurrentUser = ActionWithPayload<USER_ACTION_TYPES.SET_CURRENT_USER, UserData>;
+
+export type GoogleSigninStart = Action<USER_ACTION_TYPES.GOOGLE_SIGN_IN_START>;
+
+export type EmailSignInStart = ActionWithPayload<
+  USER_ACTION_TYPES.EMAIL_SIGN_IN_START,
+  { email: string; password: string }
+>;
+
+export type SignUpStart = ActionWithPayload<
+  USER_ACTION_TYPES.SIGN_UP_START,
+  { email: string; password: string; displayName: string }
+>;
+
+export type SignInSuccess = ActionWithPayload<USER_ACTION_TYPES.SIGN_IN_SUCCESS, UserData>;
+
+export type SignInFailed = ActionWithPayload<USER_ACTION_TYPES.SIGN_IN_FAILED, Error>;
+
+export type SignUpSuccess = ActionWithPayload<
+  USER_ACTION_TYPES.SIGN_UP_SUCCESS,
+  { user: UserData; additionalDetails: AdditionalInformation }
+>;
+
+export type SignUpFailed = ActionWithPayload<USER_ACTION_TYPES.SIGN_UP_FAILED, Error>;
+
+export type SignOutSuccess = Action<USER_ACTION_TYPES.SIGN_OUT_SUCCESS>;
+
+export type SignOutFailed = ActionWithPayload<USER_ACTION_TYPES.SIGN_OUT_FAILED, Error>;
+
+export type SignOutStart = Action<USER_ACTION_TYPES.SIGN_OUT_START>;
+
+export const setCurrentUser = withMatcher((user: UserData): SetCurrentUser => {
+  // create action returns object
+  return createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user);
+});
+
+export const checkUserSession = withMatcher((): CheckUserSession => {
+  return createAction(USER_ACTION_TYPES.CHECK_USER_SESSION);
+});
+
+export const googleSignInStart = withMatcher((): GoogleSigninStart => {
+  return createAction(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START);
+});
+
+export const emailSignInStart = withMatcher((email: string, password: string): EmailSignInStart => {
+  return createAction(USER_ACTION_TYPES.EMAIL_SIGN_IN_START, { email, password });
+});
+
+export const signInSuccess = withMatcher((user: UserData): SignInSuccess => {
+  return createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS, user);
+});
+
+export const signInFailed = withMatcher((error: Error): SignInFailed => {
+  return createAction(USER_ACTION_TYPES.SIGN_IN_FAILED, error);
+});
+
+export const signUpStart = withMatcher(
+  (email: string, password: string, displayName: string): SignUpStart => {
+    return createAction(USER_ACTION_TYPES.SIGN_UP_START, { email, password, displayName });
+  }
+);
+
+export const signUpSuccess = withMatcher(
+  (user: UserData, additionalDetails: AdditionalInformation): SignUpSuccess => {
+    return createAction(USER_ACTION_TYPES.SIGN_UP_SUCCESS, { user, additionalDetails });
+  }
+);
+
+export const signUpFailed = withMatcher((error: Error): SignUpFailed => {
+  return createAction(USER_ACTION_TYPES.SIGN_UP_FAILED, error);
+});
+
+export const signOutStart = withMatcher((): SignOutStart => {
+  return createAction(USER_ACTION_TYPES.SIGN_OUT_START);
+});
+export const signOutSuccess = withMatcher((): SignOutSuccess => {
+  return createAction(USER_ACTION_TYPES.SIGN_OUT_SUCCESS);
+});
+export const signOutFailed = withMatcher((error: Error): SignOutFailed => {
+  return createAction(USER_ACTION_TYPES.SIGN_OUT_FAILED, error);
+});
