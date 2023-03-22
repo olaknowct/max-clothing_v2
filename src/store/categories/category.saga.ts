d37f908605cebd7cf04b25cf9ab88c3e70c100d9
Christopher Olano <olanochristopher@gmail.com>
2023-03-22 15:04:56 +0800

    category sagas typed using typed redux saga

--- src/store/categories/category.saga.ts
+++ src/store/categories/category.saga.ts
@@ -1,4 +1,4 @@
-import { takeLatest, all, call, put } from 'redux-saga/effects';
+import { takeLatest, all, call, put } from 'typed-redux-saga';
 import { getCategoriesAndDocuments } from '../../utils/firebase.utils';
 import { fetchCategoriesFailed, fetchCategoriesSuccess } from './category.action';
 import { CATEGORIES_ACTION_TYPES } from './category.types';
@@ -9,12 +9,12 @@ export function* fetchCategoriesAsync() {
     // anywhere you have a function and you want to turn it into an effect use call
     // instead of await use yield.
     // yield is waiting for something to come back
-    const categoriesArray = yield call(getCategoriesAndDocuments);
+    const categoriesArray = yield* call(getCategoriesAndDocuments);
 
     // we dont call dispatch but instead use put
-    yield put(fetchCategoriesSuccess(categoriesArray));
+    yield* put(fetchCategoriesSuccess(categoriesArray));
   } catch (error) {
-    yield put(fetchCategoriesFailed(error));
+    yield* put(fetchCategoriesFailed(error as Error));
   }
 }
 
