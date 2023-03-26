93a380836055392aaf0c43417b269f60d9e1512c
Christopher Olano <olanochristopher@gmail.com>
2023-03-26 13:11:48 +0800

    media queries styles for mobile

--- src/routes/navigation/navigation.styles.tsx
+++ src/routes/navigation/navigation.styles.tsx
@@ -7,12 +7,23 @@ export const NavigationContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 25px;
+
+  @media screen and (max-width: 800px) {
+    height: 60px;
+    padding: 10px 20px;
+    margin-bottom: 20px;
+  }
 `;
 
 export const LogoContainer = styled(Link)`
   height: 100%;
   width: 70px;
   padding: 25px;
+
+  @media screen and (max-width: 800px) {
+    width: 50px;
+    padding: 0px;
+  }
 `;
 
 export const NavLinksContainer = styled.div`
@@ -29,6 +40,10 @@ export const NavLinks = styled.div`
   display: flex;
   align-items: ccenter;
   justify-content: flex-end;
+
+  @media screen and (max-width: 800px) {
+    width: 80%;
+  }
 `;
 
 export const NavLink = styled(Link)`
