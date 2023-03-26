320b7e2374c38ed53b06d5d3dfba8cfa9344f231
Christopher Olano <olanochristopher@gmail.com>
2023-03-26 12:54:07 +0800

    set up service worker pwa

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { persistor, store } from './store/store';
 
-import './index.scss';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Elements } from '@stripe/react-stripe-js';
 import { stripePromise } from './utils/stripe/stripe.utils';
 // import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -26,6 +26,8 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
